339ad1ab2a4a2996478913408cc56b74a5b08262
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2019-02-13 13:58:06 +0200

    can fetch single recipes ingredients

--- code/pehmo_server/controllers/recipes.js
+++ code/pehmo_server/controllers/recipes.js
@@ -7,7 +7,8 @@ const {
 
 const projectId = process.env.GOOGLE_PROJECT_ID;
 const api_key = process.env.API_KEY
-const url = `https://www.food2fork.com/api/search?key=${api_key}&q=`
+const url = `https://www.food2fork.com/api/search?key=${api_key}`
+const url2 = `https://www.food2fork.com/api/get?key=${api_key}`
 
 recipesRouter.get('/recipes/:ingredient', async (request, response) => {
   const translate = new Translate({
@@ -31,10 +32,24 @@ recipesRouter.get('/recipes/:ingredient', async (request, response) => {
     });
 
   try {
-    const recipes = await axios.get(url + translatedName)
-    console.log(recipes.data)
+    const recipes = await axios.get(url + '&q=' + translatedName)
+    //console.log(recipes.data)
     response.status(200).json({
       recipes: recipes.data
+    })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+recipesRouter.get('/recipes/ingredients/:recipeId', async (request, response) => {
+
+  try {
+    const ing = await axios.get(url2 + '&rId=' + request.params.recipeId)
+    //console.log(ingredients.data)
+    const ingredients = ing.data.recipe.ingredients.map(i => i)
+    response.status(200).json({
+      ingredients
     })
   } catch (error) {
     console.log(error)
