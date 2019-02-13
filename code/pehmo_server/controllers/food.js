44913486ce69b2af887fbc3965ebad5dc3da7f94
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2019-02-13 03:49:51 +0200

    food crud

--- code/pehmo_server/controllers/food.js
+++ code/pehmo_server/controllers/food.js
@@ -6,7 +6,7 @@ const url = 'http://localhost:3004/foods/'
 foodRouter.post('/user/:id/food/add', async (request, response) => {
   const body = request.body
 
-  if (body.name || body.ean || body.expiryDate || body.purchased) {
+  if (!body.name || !body.ean || !body.expiryDate || !body.purchased) {
     return response.status(400).json({
       message: `You forgot to fill in all the details!`
     })
@@ -84,14 +84,59 @@ foodRouter.get('/user/:id/food/:id2', async (request, response) => {
   }
 })
 
-foodRouter.delete('/user/:id/food/:id2', (request, response) => {
-    axios.delete(url + '?id=' + request.params.id2 + '&userId=' + request.params.id).then(
-      resp => {
-        console.log(resp.data)
-      }
-    ).catch(error => {
-      console.log(error)
+foodRouter.delete('/user/:id/food/:id2', async (request, response) => {
+  try {
+    const foods = await axios.get(url + '?userId=' + request.params.id + '&id=' + request.params.id2)
+    const listed = foods.data[0]
+
+    if (listed) {
+      const id = listed.id
+      await axios.delete(url + '?id=' + id)
+      response.status(200).json({
+        message: "Food item deleted!"
+      })
+    } else {
+      response.status(200).json({
+        message: "You haven't added any food items yet"
+      })
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+foodRouter.put('/user/:id/food/:id2', async (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.ean || !body.expiryDate || !body.purchased) {
+    return response.status(400).json({
+      message: `You forgot to fill in all the details!`
     })
+  }
+  try {
+    const foodToUpdate = await axios({
+      method: 'put',
+      url: url + '?userId=' + request.params.id + '&id=' + request.params.id2,
+      data: {
+        name: body.name,
+        ean: body.ean,
+        imageUrl: `https://foodieimages.s3.amazonaws.com/images/entries/180x220/${body.ean}_0.png`,
+        expiryDate: body.expiryDate,
+        purchased: body.purchased,
+        userId: request.params.id
+      },
+      config: {
+        headers: {
+          'Content-Type': 'aplication/json'
+        }
+      }
+    })
+    response.status(200).json({
+      message: `Food item ${body.name} updated succesfully`
+    })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = foodRouter
